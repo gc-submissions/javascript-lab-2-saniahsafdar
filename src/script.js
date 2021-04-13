7d9abb0f0358992b758b22e297e64b820a9cba8b
Saniah Safdar <77857761+saniahsafdar@users.noreply.github.com>
2021-04-13 08:47:16 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,53 @@
 "use strict";
 
-// TODO - write your code here.
+const randomDamage = () => {
+    return Math.floor(Math.random() * 10) + 1;
+} 
+
+const chooseOption = (opt1,opt2) => {
+    let randNum = Math.round(Math.random());
+    return (randNum === 0? opt1 : opt2); 
+}   
+
+
+function attackPlayer(health){
+   return (health - randomDamage());
+}
+
+const logHealth = (player, health) =>{
+       console.log(`${player} health: ${health}`);
+}
+
+const logDeath = (winner, loser) =>{
+    console.log(`${winner} defeated ${loser}`);
+}
+
+const isDead = (health) =>{
+    return (health <= 0 ? true: false);
+}
+
+function fight(player1,player2,player1Health,player2Health) {
+    while (true){
+    let attacker= chooseOption(player1, player2);
+    if (attacker === player1) {
+        player2Health = attackPlayer(player2Health);
+        logHealth(player2,player2Health);
+       if (isDead(player2Health)){
+           logDeath(player1,player2);
+           break;
+       }  
+    } 
+    else
+    {
+        player1Health = attackPlayer(player1Health);
+        logHealth(player1,player1Health);
+       if (isDead(player1Health)){
+           logDeath(player2,player1);
+           break;
+       }
+    }
+    
+    } 
+}
+
+fight("Saniah", "Anber", 100, 100);
